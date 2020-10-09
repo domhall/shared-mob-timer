a0d41c79033c0a0ff3c0853e46e9f22f70813946
domhall <dhall@worth.systems>
2020-10-09 15:07:58 +0100

    Timer attempt

---
+++ src/components/Timer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="timer">
+    <h1>{{ time.format('mm:ss') }}</h1>
+    <h1>{{ endTime.format('mm:ss') }}</h1>
+    <h1>{{ timeLeft }}</h1>
+  </div>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+  name: 'Timer',
+  data() {
+    return {
+      time: '',
+      endTime: '',
+      timeLeft: '',
+    };
+  },
+  mounted() {
+    this.endTime = moment().add(5, 'm');
+    setInterval(() => {
+      this.time = moment();
+      // this.timeLeft = moment(moment.duration(this.endTime - moment())).format('mm:ss');
+      this.timeLeft = moment().to(this.endTime);
+    }, 10);
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -2,15 +2,18 @@ import Vue from 'vue';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+import moment from 'moment';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 
+Vue.prototype.moment = moment;
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  moment,
   render: (h) => h(App),
 }).$mount('#app');
